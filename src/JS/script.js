77b747e654e42cc7bc61130b2b003f0a484619c4
Dmitriy Antonov <dmitrijantonov850@gmail.com>
2024-10-23 11:45:36 +0300

    Начать делать О нас

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,27 +1,12 @@
 "use strict"
 
-// let menu_active = false;
-
-// const show_menu = (action) => {
-//     menu = document.getElementById('menu');
-
-//     if (menu_active | action == "close"){
-//         menu.style.left = "-600px";
-//         menu_active = false;
-//     }
-//     else{
-//         menu.style.left = "0px";
-//         menu_active = true;
-//     }
-// }
-
 let menu_active = false;
 
-const show_menu = () => {
+function show_menu() {
     let menu_li = document.getElementById('menu-li');
 
     if (menu_active == false) {
-        menu_li.style.left = "-500px";
+        menu_li.style.left = "-460px";
         menu_li.style.opacity = "100";
         menu_active = true;
     } else {
