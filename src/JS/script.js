9d344db3693202b3bb2fc7a0fe392c541303a73d
Dmitriy-Antonov <dmitrijantonov850@gmail.com>
2024-10-26 00:04:42 +0300

    Начать делать меню

--- src/JS/script.js
+++ src/JS/script.js
@@ -7,7 +7,7 @@ function show_menu() {
 
     if (innerWidth >= 1100) {
         if (menu_active == false) {
-            menu_li.style.left = "-460px";
+            menu_li.style.left = "-220px";
             menu_li.style.opacity = "100";
             menu_active = true;
         } else {
@@ -40,31 +40,30 @@ function show_menu() {
 
 function calorie_calculation () {
     
-    let age = document.getElementById('ages').value;
-    let weight = document.getElementById('weight').value;
-    let height = document.getElementById('height').value;
-    let activity_ratio = document.getElementById('activity-ratio').value;
+    let age = +document.getElementById('ages').value;
+    let weight = +document.getElementById('weight').value;
+    let height = +document.getElementById('height').value;
+    let activity_ratio = +document.getElementById('activity-ratio').value;
     let result1 = 0;
     
     if (document.getElementById('male-gender').checked == true) {
-        result1 = (10 * weight + 6,25 * height - 5 * age - 161) * activity_ratio;
+        result1 = (10 * weight + 6.25 * height - 5 * age - 161) * activity_ratio;
     } else if (document.getElementById('female-gender').checked == true){
-        result1 = (10 * weight + 6,25 * height - 5 * age + 5) * activity_ratio;
+        result1 = (10 * weight + 6.25 * height - 5 * age + 5) * activity_ratio;
     } else {
         alert("Выберите пол!")
     }
-debugger
 
     if (document.getElementById('weight-gain').checked == true) {
-        result1 += result1 / 100 * 20;
+        result1 += result1 * 0.2;
     } else if (document.getElementById('slow-weight-gain').checked == true) {
-        result1 += result1 / 100 * 10;
+        result1 += result1 * 0.1;
     } else if (document.getElementById('maintaining-weight').checked == true) {
         result1 = result1;
     } else if (document.getElementById('slow-weight-loss').checked == true) {
-        result1 -= result1 / 100 * 10; 
+        result1 -= result1 * 0.1; 
     } else if (document.getElementById('weight-loss').checked == true) {
-        result1 -= result1 / 100 * 20;
+        result1 -= result1 * 0.2;
     } else {
         alert ("Вы не указали Вашу цель!");
     }
